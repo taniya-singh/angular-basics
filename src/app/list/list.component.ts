74b636dd4375404784710ecf80c064fbad8a54f1
demonicinn <Info.gurpreetsaini@gmail.com>
2021-05-22 22:34:01 +0530

    my first commit

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  public arr:any=[]
+  public aniket:number=0
+  constructor() { }
+
+  ngOnInit(): void {
+  this.arr=[{ id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }]
+  }
+}
